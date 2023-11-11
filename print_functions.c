e39eca1061af6124c48c3fe49b91afe4f76ccadb
keroo joseph <143596572+keroojoseph@users.noreply.github.com>
2023-11-11 15:31:05 +0200

    Update print_functions.c

--- print_functions.c
+++ print_functions.c
@@ -4,7 +4,7 @@
 
 /**
  * print_char - Prints a char
- * by Mark and Kero
+ * by Mark and Keroles
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
