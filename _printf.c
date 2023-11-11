02256264317382f1d6c8bbd4fba516b7c65cde3f
keroo joseph <143596572+keroojoseph@users.noreply.github.com>
2023-11-11 15:24:37 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -5,7 +5,7 @@ void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format.
- * by Mark and Kero
+ * by Mark and Keroles
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
